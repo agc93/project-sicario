6286c00876b4472d39182efc99037d4490cf2f3e
Alistair Chapman <alistair@agchapman.com>
2021-01-15 23:59:40 +1000

    Initial commit

---
+++ src/SicarioPatch.Core/ArchiveHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace SicarioPatch.Core
+{
+    public static class ArchiveHelpers
+    {
+        public static FileInfo ToZipFile(this DirectoryInfo dirInfo)
+        {
+            var path = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.zip");
+            ZipFile.CreateFromDirectory(dirInfo.FullName, path);
+            return new FileInfo(path);
+        }
+    }
+}
---
+++ src/SicarioPatch.Core/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using BuildEngine;
+using HexPatch;
+using HexPatch.Build;
+using MediatR;
+
+namespace SicarioPatch.Core
+{
+    public class ModRequest : IRequest<FileInfo>
+    {
+        public ModRequest(Dictionary<string, Mod> mods)
+        {
+            Mods = mods;
+        }
+
+        public Dictionary<string, Mod> Mods { get; }
+        public bool PackResult { get; set; } = false;
+    }
+
+    public class PatchRequestHandler : IRequestHandler<ModRequest, FileInfo>
+    {
+        private readonly ModPatchServiceBuilder _builder;
+
+        public PatchRequestHandler(ModPatchServiceBuilder servBuilder)
+        {
+            _builder = servBuilder;
+        }
+        
+        public async Task<FileInfo> Handle(ModRequest request, CancellationToken cancellationToken)
+        {
+            var mpServ = await _builder.GetPatchService(request.Mods);
+            await mpServ.LoadFiles().RunPatches();
+            (bool Success, FileInfo Result)? result;
+            if (request.PackResult)
+            {
+                result = mpServ.RunBuild(ctx =>
+                    new FileInfo(
+                        Path.Combine(ctx.BuildScript.WorkingDirectory, $"merged-{DateTime.UtcNow.Ticks}_P.pak")));
+            }
+            else
+            {
+                result = mpServ.RunAction(ctx => ctx.WorkingDirectory.ToZipFile());
+            }
+
+            return result != null ? (result.Value.Success ? result.Value.Result : null) : null;
+        }
+    }
+}
