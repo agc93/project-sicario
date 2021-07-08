c6c84332168c3da205b8d209a57fcc8f133fa90c
Alistair Chapman <alistair@agchapman.com>
2021-07-08 16:53:41 +1000

    Add loader project;

---
+++ src/SicarioPatch.Loader/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SicarioPatch.Loader
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
