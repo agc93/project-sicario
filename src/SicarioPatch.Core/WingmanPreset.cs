17d98c9627a39e0ee3fef88fdf3ae8abcfb39f8e
Alistair Chapman <alistair@agchapman.com>
2021-07-08 17:48:06 +1000

    Add preset type;

---
+++ src/SicarioPatch.Core/WingmanPreset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SicarioPatch.Core
+{
+    public class WingmanPreset
+    {
+        public int Version { get; set; } = 1;
+        public Dictionary<string, string> ModParameters { get; set; }
+        public List<WingmanMod> Mods { get; set; }
+    }
+}
