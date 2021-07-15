7e6f19ebbefb9e4619c7663b8868961bd6e415a0
Alistair Chapman <alistair@agchapman.com>
2021-07-15 22:59:26 +1000

    Improve duplicate handling in duplicateEntry;

--- src/SicarioPatch.Assets/Patches/DuplicateItemPatchType.cs
+++ src/SicarioPatch.Assets/Patches/DuplicateItemPatchType.cs
@@ -8,25 +8,32 @@ using UAssetAPI.StructTypes;
 
 namespace SicarioPatch.Assets.Patches
 {
-    public class DuplicateItemPatchType : AssetPatchType<(string SourceName, string TargetName, int Index)>
+    public class DuplicateItemPatchType : AssetPatchType<(string SourceName, int SourceIndex, string TargetName, int Index)>
     {
         public override string Type => "duplicateEntry";
         protected override IEnumerable<AssetInstruction>? RunPatch(IEnumerable<PropertyData> inputProperties,
-            (string SourceName, string TargetName, int Index) parsedValue) {
+            (string SourceName, int SourceIndex, string TargetName, int Index) parsedValue) {
             var newOutcomes = new Dictionary<string, PropertyData>();
-            foreach (var property in inputProperties.Where(ip => ip is  StructPropertyData).Cast<StructPropertyData>().Where(v => v.Name == parsedValue.SourceName)) {
-                var newProp = new StructPropertyData(parsedValue.TargetName, property.Asset) {Value = property.Value};
-                newOutcomes.Add(parsedValue.Index > 0 ? parsedValue.Index.ToString() : parsedValue.TargetName, newProp);
+            var properties = inputProperties.Where(ip => ip is StructPropertyData).Cast<StructPropertyData>()
+                .Where(v => v.Name == parsedValue.SourceName).ToList();
+            foreach (var property in properties) {
+                if (properties.IndexOf(property) == parsedValue.SourceIndex) {
+                    var newProp = new StructPropertyData(parsedValue.TargetName, property.Asset)
+                        {Value = property.Value};
+                    newOutcomes.Add(parsedValue.Index > 0 ? parsedValue.Index.ToString() : parsedValue.TargetName,
+                        newProp);
+                }
             }
 
             return new[] {new AssetInstruction {Type = InstructionType.Add, FixedProperties = newOutcomes}};
         }
 
-        protected override Parser<(string SourceName, string TargetName, int Index)> ValueParser => 
+        protected override Parser<(string SourceName, int SourceIndex, string TargetName, int Index)> ValueParser => 
             Parsers.Terms.String()
+                .And(Parsers.ZeroOrOne(Parsers.Terms.Char('[').SkipAnd(Parsers.Terms.Integer()).AndSkip(Parsers.Terms.Char(']'))).Then(zo => Convert.ToInt32(zo)))
                 .AndSkip(Parsers.Terms.Char('>'))
                 .And(Parsers.Terms.String())
                 .And(Parsers.ZeroOrOne(Parsers.Terms.Char('(').SkipAnd(Parsers.Terms.Integer()).AndSkip(Parsers.Terms.Char(')'))).Then(zo => Convert.ToInt32(zo)))
-                .Then(x => (SourceName: x.Item1.ToString(), TargetName: x.Item2.ToString(), Index: x.Item3));
+                .Then(x => (SourceName: x.Item1.ToString(), SourceIndex: x.Item2, TargetName: x.Item3.ToString(), Index: x.Item4));
     }
 }
